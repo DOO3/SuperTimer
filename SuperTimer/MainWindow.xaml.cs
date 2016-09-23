d2a8c8184f9d62ce8a6ff8c66dffda52067c06b0
DOO3 <son.rohan@ukr.net>
2016-09-23 19:44:38 +0300

    Add project files.

---
+++ SuperTimer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuperTimer
+{
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, Color> Colors = new Dictionary<string, Color>
+        {
+            { "Black", new Color {R = 0, G = 0, B = 0 } },
+            { "Red", new Color {R = 255, G = 0, B = 0 } },
+            { "Green", new Color {R = 0, G = 255, B = 0 } },
+            { "Blue", new Color {R = 0, G = 0, B = 255 } }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbFrom.ItemsSource = cbTo.ItemsSource = Colors.Keys;
+        }
+
+        private void bStart_Click(object sender, RoutedEventArgs e)
+        {
+            AddTimer();
+        }
+
+        private void AddTimer()
+        {
+            wpTimes.Children.Add(new Timer(new TimeSpan(int.Parse(tbHour.Text), int.Parse(tbMinut.Text), int.Parse(tbSecond.Text)), tbName.Text));
+        }
+    }
+}
---
+++ SuperTimer/Timer.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SuperTimer
+{
+    public partial class Timer : UserControl
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        TimeSpan hms = new TimeSpan();
+
+        public Timer(TimeSpan ts, string name, Color from, Color to)
+        {
+            InitializeComponent();
+
+            lName.Content = name;
+            hms = ts;
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lTimer.Content = hms.ToString();
+
+            if (hms.Equals(TimeSpan.FromTicks(0)))
+            {
+                MessageBox.Show($"Timer {lName.Content} ends!", "End", MessageBoxButton.OK, MessageBoxImage.Information);
+                timer.Stop();                
+            }
+
+            hms -= TimeSpan.FromSeconds(1);
+        }
+
+        
+    }
+}
